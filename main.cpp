63a92b6a2f828a112b3a0db24da03453320457e1
Megan Jensen <spottedeye2@gmail.com>
2025-03-06 14:09:55 -0600

    Created the sqrt function

--- main.cpp
+++ main.cpp
@@ -9,7 +9,20 @@ double power(double num, int power) {
   return num;
 }
 
+double sqrt(double num){
+    double x = num;
+    double y = 1;
+    
+    while (x - y > 0) {
+        x = (x + y)/2;
+        y = num/x;
+    }
+    
+    return x;
+}
+
 int main() {
   cout << "Github Test" << endl;
   cout << power(10, 2) << endl;
+  cout << sqrt(100) << endl;
 }
